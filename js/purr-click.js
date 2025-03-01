8c95a663afd0212629e34b0f6536552aeffdf1f8
Eugene Wekesa <eugenco578@gmail.com>
2025-03-01 22:54:23 +0300

    used css selectors

--- js/purr-click.js
+++ js/purr-click.js
@@ -1,8 +1,8 @@
-$(function(){
+$(function() {
+  // Get the audio player element
   var player1 = document.getElementById('player1');
-  $('img').click(function(){
+  $('img').click(function() {
+    // Play the audio when an image is clicked
     player1.play();
   });
 });
-
-
